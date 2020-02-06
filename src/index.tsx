4b081b1d4da287f945a718afe341d3428d47b559
Michael Fosker <mikefosker@Michaels-MBP.headforwards.com>
2020-02-06 15:33:11 +0000

    Auto login, but need to clean up all the login code now.

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AzureAD } from 'react-aad-msal';
+import { authProvider } from './utils/authProvider';
 import './index.css';
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <AzureAD provider={authProvider} forceLogin={true}>
+        <App />
+    </AzureAD>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
